7b16b5a97ba7e654d154de9e0309326b27d783b6
Alejandro <alejandromartinezburgos01@gmail.com>
2022-04-09 11:07:57 +0200

    [1]-generic-error-when-receiving-list-of-users

---
+++ tests/app/Infrastructure/Controller/GetUsersListControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\app\Infrastructure\Controller;
+
+use App\Application\UserDataSource\UserDataSource;
+use App\Domain\User;
+use Exception;
+use Illuminate\Http\Response;
+use Mockery;
+use Tests\TestCase;
+
+class GetUsersListControllerTest extends TestCase
+{
+
+    private UserDataSource $userDataSource;
+    /**
+     * @setUp
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->userDataSource = Mockery::mock(UserDataSource::class);
+        $this->app->bind(UserDataSource::class, fn () => $this->userDataSource);
+
+
+    }
+
+    /**
+     * @test
+     */
+    public function error_occurs_when_asked_for_users_list()
+    {
+        $this->userDataSource
+            ->expects('returnUsersList')
+            ->with()
+            ->never()
+            ->andThrow(new Exception('An error occurred while a petition was made'));
+
+        $response = $this->get('/api/users/list');
+
+        $response->assertExactJson(['error' => 'An error occurred while a petition was made']);
+    }
+
+}
