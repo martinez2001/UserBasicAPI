702cfc94c8015aca9f35187d8043476145aebfed
Rubén Iriso <79251393+rubeniriso@users.noreply.github.com>
2022-04-11 11:40:37 +0200

    2 as client i want to get user data (#4)
    * [2] - Generic error test when receiving user data passed
    
    * [2] - User found in User Data Service test passed
    
    * [2] - User with given id exists in GetDataController passed
    
    * [2] - Generic error during petition in GetDataController test passed
    
    * [2] - Snake edge format for tests fix

---
+++ app/Infrastructure/Controllers/GetUserDataController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Infrastructure\Controllers;
+
+use App\Application\GetUserData\GetUserDataService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Exception;
+use Illuminate\Routing\Controller as BaseController;
+
+class GetUserDataController extends BaseController
+{
+    private GetUserDataService $getUserDataService;
+    public function __construct(GetUserDataService $getUserDataService){
+        $this->getUserDataService = $getUserDataService;
+    }
+
+    public function __invoke(string $userId): JsonResponse
+    {
+        try{
+            $userData = $this->getUserDataService->execute($userId);
+        }catch(Exception $exception) {
+            if ($exception->getMessage() === 'User not found') {
+                return response()->json([
+                    'error' => "user does not exist"
+                ], Response::HTTP_BAD_REQUEST);
+            }
+            else {
+                return response()->json([
+                    'error' => "Petition error"
+                ], Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        }
+        return response()->json([
+            'user' => $userData
+        ], Response::HTTP_OK);
+    }
+}
