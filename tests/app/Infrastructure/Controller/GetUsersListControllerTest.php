1596020f271ef5c4b7c91f01654c88f11b749032
Alejandro <alejandromartinezburgos01@gmail.com>
2022-04-11 11:21:52 +0200

    [1] - Receiving happy path users list of Controller, Test Passed

--- tests/app/Infrastructure/Controller/GetUsersListControllerTest.php
+++ tests/app/Infrastructure/Controller/GetUsersListControllerTest.php
@@ -57,6 +57,48 @@ class GetUsersListControllerTest extends TestCase
             $response->assertStatus(Response::HTTP_OK)->assertExactJson(['list' => array()]);
     }
 
+    /**
+     * @test
+     */
+    public function list_with_3_users_returned(){
+        /*
+        $user1 = new User(1, 'email1@gmail.com');
+        $user1->userId = $user1->getId();
+        $users_list[] = $user1;
+
+        $user2 = new User(2, 'email2@gmail.com');
+        $user2->userId = $user2->getId();
+        $users_list[] = $user2;
+
+        $user3 = new User(3, 'email3@gmail.com');
+        $user3->userId = $user1->getId();
+        $users_list[] = $user3;
+        *
+         * Since the statement shows that the response to the happy path is a list of Ids and not Users themselves, I put it like this
+         */
+
+        $userId1 = 1;
+        $users_list[] = $userId1;
+
+        $userId2 = 2;
+        $users_list[] = $userId2;
+
+        $userId3 = 3;
+        $users_list[] = $userId3;
+
+        $this->userDataSource
+            ->expects('getUsersList')
+            ->with()
+            ->once()
+            ->andReturn($users_list);
+
+        $response = $this->get('/api/users/list');
+
+        $response->assertStatus(Response::HTTP_OK)->assertExactJson(['list' => array($userId1,$userId2,$userId3)]);
+    }
+
+
+
 
 
 
