fd89f35bc21f30f08ae12acf2598224eb73bfe20
rubeniriso <rubenirisoe@gmail.com>
2022-04-11 12:22:38 +0200

    [2] - Fake User Data Source added with prior master pull

--- app/Infrastructure/Providers/AppServiceProvider.php
+++ app/Infrastructure/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Infrastructure\Providers;
 
 use App\Application\UserDataSource\UserDataSource;
+use Tests\app\Doubles\FakeUserDataSource;
 use App\DataSource\Database\EloquentUserDataSource;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,8 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//        $this->app->bind(UserDataSource::class, function () {
-//            return new EloquentUserDataSource();
-//        });
+        $this->app->bind(UserDataSource::class, function () {
+            return new FakeUserDataSource();
+        });
     }
 }
